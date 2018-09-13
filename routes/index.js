a90825da78a521a901a1b1194377c0dc4fbc11d5
Christopher Adams <chrismichaeladams@gmail.com>
2018-09-13 18:27:49 -0400

    Add tracking for Google Analytics

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+
+config = require('../config');
+configLocal = require('../config.local');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'IOOS OSMC' });
+  res.render('index', { title: 'IOOS OSMC', googleAnalyticsID: config.googleAnalyticsID });
 });
 
 module.exports = router;
